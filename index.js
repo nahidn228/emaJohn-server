1ee4ff2b684a6eb7a1010cc4bf15280079966f80
Nahid Hasan <nahidn228@gmail.com>
2024-12-17 21:36:23 +0600

    Use Post to load some products using keys

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dssil.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -40,6 +40,19 @@ async function run() {
         .skip(pages * size)
         .limit(size)
         .toArray();
+      res.send(result);
+    });
+
+    app.post("/productByIds", async (req, res) => {
+      const ids = req.body;
+      const idsWithObjectId = ids.map((id) => new ObjectId(id));
+      console.log(idsWithObjectId);
+      const query = {
+        _id: {
+          $in: idsWithObjectId,
+        },
+      };
+      const result = await productCollection.find(query).toArray();
       res.send(result);
     });
 
